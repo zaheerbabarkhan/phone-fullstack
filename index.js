1778683648843724e909e713bd5315011c7d5f0a
Zaheer Babar <zaheerbaberkhan@gmail.com>
2025-09-10 04:14:32 -0700

    initial code for the backend and the frontend

--- index.js
+++ index.js
@@ -72,7 +72,8 @@ app.get("/api/persons/:id", (request, response) => {
 app.delete("/api/persons/:id", (request, response) => {
     const id = request.params.id
     persons = persons.filter(person => person.id !== id)
-
+    console.log("this is here")
+    console.log(persons)
     return response.status(204).end()
 })
 
